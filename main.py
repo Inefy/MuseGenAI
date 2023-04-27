4f33c40fb165b5f5a632a8ff79eb48e3f9582039
Inefy <zbatten@mun.ca>
2023-04-27 18:50:58 -0230

    fixing output

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import openai
 import config
 import mido
 from mido import Message, MidiFile, MidiTrack
+import fractions
 
 # Initialize OpenAI API (replace 'your_api_key' with your actual API key)
 openai.api_key = config.API_KEY
@@ -26,14 +27,20 @@ def notation_to_midi(notation, output_file):
     midi.tracks.append(track)
 
     for line in notation:
-        duration, pitch = line.split()
-        duration = int(duration)
-        note = mido.note_name_to_number(pitch)
-
-        track.append(Message('note_on', note=note, velocity=64, time=0))
-        track.append(Message('note_off', note=note, velocity=64, time=int(480 * (4 / duration))))
+        try:
+            duration, pitch = line.split()
+            duration = round(4 * float(fractions.Fraction(duration)))
+            note = mido.note_name_to_int(pitch)
+            track.append(Message('note_on', note=note, velocity=64, time=0))
+            track.append(Message('note_off', note=note, velocity=64, time=int(480 * (4 / duration))))
+        except ValueError:
+            print(f"Skipping line: {line}")
+        except mido.KeySignatureError:
+            print(f"Skipping line due to an invalid pitch: {line}")
 
     midi.save(output_file)
+
+
 
 # User input for parameters
 engine_choice = input("Choose the GPT engine (gpt-4, gpt-3, gpt-3-turbo): ")
@@ -45,13 +52,13 @@ temperature = float(input("Enter the creativity level (0.1-1.0, higher is more c
 
 # Set engine name based on user input
 engine_name = {
-    "gpt-4": "text-davinci-002",
-    "gpt-3": "davinci",
-    "gpt-3-turbo": "davinci-codex",
-}.get(engine_choice.lower(), "text-davinci-002")  # Default to GPT-4 if input is not recognized
+    "gpt-4": "gpt-4",
+    "gpt-3": "text-davinci-003",
+    "gpt-3-turbo": "gpt-3.5-turbo",
+}.get(engine_choice.lower(), "gpt-3.5-turbo")  # Default to GPT-3 Turbo (gpt-3.5-turbo) if input is not recognized
 
 # Generate music notation
-prompt = f"Generate a {style} {genre} piano melody in the key of {key}:"
+prompt = f"Generate a {style} {genre} piano melody with individual notes in the key of {key}. Provide the melody in the following format: 'duration pitch', with each note on a separate line."
 notation = generate_music_notation(engine_name, prompt, num_notes, temperature)
 
 # Convert music notation to MIDI
