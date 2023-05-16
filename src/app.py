efe1f01fc2455c31383b812ac9a2f436b2473bd1
Inefy <zbatten@mun.ca>
2023-05-16 18:02:51 -0230

    updated app.py to supported multiple instruments

--- src/app.py
+++ src/app.py
@@ -28,19 +28,22 @@ def generate_midi():
         "gpt-3-turbo": "gpt-3.5-turbo",
     }.get(data['engine'].lower(), "gpt-3.5-turbo")
 
-    prompt = f"Generate a {data['style']} {data['genre']} {data['instrument']} song with individual notes and chords. Provide the melody in the following format: 'duration pitch1 pitch2 ...', with each note or chord on a separate line."
-
-    notation = main.generate_music_notation(
-        engine_name,
-        prompt,
-        int(data['num_notes']),
-        float(data['temperature'])
-    )
+    notations = []
+    for instrument in data['instruments']:
+        prompt = f"Generate a {data['style']} {data['genre']} {instrument} song with individual notes and chords. Provide the melody in the following format: 'duration pitch1 pitch2 ...', with each note or chord on a separate line."
+        notation = main.generate_music_notation(
+            engine_name,
+            prompt,
+            int(data['num_notes']),
+            float(data['temperature'])
+        )
+        notations.append(notation)
 
     _, temp_path = tempfile.mkstemp(suffix=".mid")
-    main.notation_to_midi(notation, temp_path, data['instrument'])
+    main.notation_to_midi(notations, temp_path, data['instruments'])
     
     return send_file(temp_path, mimetype="audio/midi", as_attachment=True, attachment_filename="generated_music.mid")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
